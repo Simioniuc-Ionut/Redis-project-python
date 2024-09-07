285bda97571ccfac969f0e348c1a51661e7d21c6
Simioniuc Ionut-Mihai <simioniucionut@gmail.com>
2024-09-07 15:17:33 +0300

    stage 3

--- app/utils/Decorators.py
+++ app/utils/Decorators.py
@@ -1,9 +1,9 @@
-# app/utils/decorators.py
 
 def debug(func):
     def wrapper(*args, **kwargs):
-        print(f"Calling {func.__name__} with args: {args} kwargs: {kwargs}")
+        class_name = args[0].__class__.__name__ if args else ''
+        print(f"Calling {class_name}.{func.__name__} with args: {args[1:]} kwargs: {kwargs}")
         result = func(*args, **kwargs)
-        print(f"{func.__name__} returned: {result}")
+        print(f"{class_name}.{func.__name__} returned: {result}")
         return result
     return wrapper
