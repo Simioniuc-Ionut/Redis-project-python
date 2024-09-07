a5dad8ee52c822928753ced53cf6a35656464cc0
Simioniuc Ionut-Mihai <simioniucionut@gmail.com>
2024-09-07 20:44:47 +0300

    stage 4 , event loop

--- app/EventLoop.py
+++ app/EventLoop.py
@@ -17,21 +17,22 @@ class EventLoop:
         return asyncio.get_event_loop().sock_recv(self.client_socket, 1024)
 
     def send_message_to_receiver(self, message):
-         self._mediator.notify_receiver(message)
+        self._mediator.notify_receiver(message)
 
     async def start_task(self):
         self.__initiate()
         print("Event Loop started")
         while self._is_running:  # here manage communication between server - client
-            message = self.wait_message_from_receiver()  # wait message from client
-            if not message:  # connection is closed
-                print("Client disconnected")
-                self.client_socket.close()
-                self._is_running = False
-            else:
-                # debug
-                # print(f"Message received: {message}")
-                self.send_message_to_receiver(message)
+            message = await self.wait_message_from_receiver()  # wait message from client
+            if message:  # connection is closed
+                if message == b"":
+                    print("Client disconnected")
+                    self.client_socket.close()
+                    self._is_running = False
+                else:
+                    # debug
+                    # print(f"Message received: {message}")
+                    self.send_message_to_receiver(message)
 
     def __initiate(self):
         self._mediator = Mediator()
