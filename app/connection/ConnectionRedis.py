d7abeab621c40ae4c9a493f7b7888070bf765ce1
Simioniuc Ionut-Mihai <simioniucionut@gmail.com>
2024-09-07 20:08:10 +0300

    stage 4 , event loop

--- app/connection/ConnectionRedis.py
+++ app/connection/ConnectionRedis.py
@@ -14,7 +14,8 @@ class ConnectionRedis:
 
     async def accept_client(self):
         # Trebuie să fie asincronă
-        self._instance._client_socket, _address =  self._instance._server_socket.accept()  # wait for client
+        self._instance._client_socket, _address =\
+            asyncio.get_event_loop().sock_accept(self._instance._server_socket)  # wait for client
         print("Client connected", _address)
         return self._instance._client_socket  # returnează socketul clientului
          # old
