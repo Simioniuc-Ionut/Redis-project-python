be430ae6b026b907fba7f51a6d97b826351108b0
Simioniuc Ionut-Mihai <simioniucionut@gmail.com>
2024-09-07 20:01:41 +0300

    stage 4 , event loop

--- app/main.py
+++ app/main.py
@@ -12,10 +12,8 @@ import socket  # noqa: F401
 async def main_loop(server_set):
     while True:  # manage all connections in concurrent way
         client_socket = server_set.accept_client()  # asincron wait for client
-        if client_socket is not None:
-            print("Client connected " , client_socket, " ", server_set)
-            loop = EventLoop(client_socket)
-            asyncio.create_task(loop.start_task())  # asincron start task
+        loop = EventLoop(client_socket)
+        asyncio.create_task(loop.start_task())  # asincron start task
 
 
         # daca clientu lse deconcteaza apelez opresc loop ul si inchid clientul
