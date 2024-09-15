9a26b235d310710ee6d548fcdfa4d47228938e3b
Simioniuc Ionut <simioniucionut@gmail.com>
2024-09-15 15:57:45 +0300

    codecrafters submit [skip ci]

--- app/command_pattern/commands/CommandInfo.py
+++ app/command_pattern/commands/CommandInfo.py
@@ -8,9 +8,13 @@ class CommandInfo(Command):
         self.receiver = receiver
         self.role = Globals.global_role
         self.port = Globals.global_port
+        self.master_replied = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb"  # provizorii
+        self.master_repl_offset = 0  # provizorii
 
     async def execute(self):
-        if self.role is not "master":
+        if self.role != "master":
             self.role = "slave"
-        message = f"${str(len(self.role) + 5)}\r\nrole:{self.role}\r\n"
+        message = f"role:{self.role}\r\nmaster_repl_offset:{self.master_repl_offset}\r\nmaster_replid:{self.master_replied}"
+        message = "$" + str(len(message)) + "\r\n" + message + "\r\n"
+        print("Message:", message)
         await self.receiver.send_message(message.encode())
