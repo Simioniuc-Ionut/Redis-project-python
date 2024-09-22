a6fe58e0b2928840dca11e9b7f2f4c501d98c243
Simioniuc Ionut <simioniucionut@gmail.com>
2024-09-22 12:05:25 +0300

    codecrafters submit [skip ci]

--- app/command_pattern/ProcessCommandsToReplicas.py
+++ app/command_pattern/ProcessCommandsToReplicas.py
@@ -13,6 +13,14 @@ async def process_propagation_send_SET_command_to_replicas(key, value):
             print("Sending SET command to replica")
             await replica.send_message(message.encode())
 
+async def propagate_wait_command_to_replicas(replicas_connections, milliseconds):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(send_and_wait_for_ack, replica, milliseconds) for replica in replicas_connections]
+        results = [future.result() for future in concurrent.futures.as_completed(futures)]
+
+    print("Results: ", results)
+    return sum(results)  # Count the number of successful responses
+
 
 def send_and_wait_for_ack(replica, milliseconds):
     def run_in_thread():
@@ -23,49 +31,16 @@ def send_and_wait_for_ack(replica, milliseconds):
             loop.run_until_complete(replica.send_message(response.encode()))
             print(f"Sent MasterGetAck command to replica")
 
-            data = loop.run_until_complete(asyncio.wait_for(replica.receive_message(), timeout=milliseconds / 1000))  # wait for the response from the replica
+            data = loop.run_until_complete(asyncio.wait_for(replica.receive_message(),
+                                                            timeout=milliseconds / 1000))  # wait for the response from the replica
             if data:
                 return 1
         except asyncio.TimeoutError:
             print("Timeout error: Did not receive response from replica in time")
         except Exception as e:
-            print(f"Error sending MasterGetAck command to replica: {e}")
+            print(f"Error sending MasterGetAck command to repl ica: {e}")
         finally:
             loop.close()
         return 0
 
     return run_in_thread()
-
-async def propagate_wait_command_to_replicas(replicas_connections, milliseconds):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [executor.submit(send_and_wait_for_ack, replica, milliseconds) for replica in replicas_connections]
-        results = [future.result() for future in concurrent.futures.as_completed(futures)]
-
-    print("Results: ", results)
-    return sum(results)  # Count the number of successful responses
-
-# async def propagate_wait_command_to_replicas(replicas_connections, milliseconds):
-#     async def send_and_wait_for_ack(replica,milliseconds):
-#         try:
-#             response = f"*3\r\n$8\r\nREPLCONF\r\n$6\r\nGETACK\r\n$1\r\n*\r\n"
-#             await replica.send_message(response.encode())
-#             print(f"Sent MasterGetAck command to replica")
-#             if milliseconds < 1000:
-#                 milliseconds += 3500
-#             data = await asyncio.wait_for(replica.receive_message(), timeout=milliseconds / 1000)  # wait for the response from the replica
-#             # print(f"Received response from replica: {data}")
-#             if data:
-#                 return 1
-#         except asyncio.TimeoutError:
-#             print("Timeout error: Did not receive response from replica in time")
-#         except Exception as e:
-#             print(f"Error sending MasterGetAck command to replica: {e}")
-#         return 0
-#
-#     tasks = [asyncio.create_task(send_and_wait_for_ack(replica,milliseconds)) for replica in replicas_connections]
-#     results = await asyncio.gather(*tasks, return_exceptions=True)
-#
-#
-#     print("Results: ", results)
-#     return sum(results)  # Count the number of successful responses
-
